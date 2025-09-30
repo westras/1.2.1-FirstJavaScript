4e3198bc8d3b7f12d1edca76713f21a1a193e53e
westras <r.westholm@telia.com>
2025-09-30 13:34:29 +0200

    first save

---
+++ script.js
@@ -0,0 +1,25 @@
+
+ const btn=document.getElementById("btn");
+ const ruta=document.getElementById("ruta");
+const skit=document.getElementById("skit");
+  
+btn.addEventListener('click', function(){ 
+  ruta.classList.add("synligt");
+  ruta.classList.remove("osynligt");
+  btn.classList.remove("synligt");
+  btn.classList.add("osynligt");
+});
+
+ruta.addEventListener('click', function(){ 
+  skit.classList.add("synligt");
+  skit.classList.remove("osynligt");
+  ruta.classList.remove("synligt");
+  ruta.classList.add("osynligt");
+});
+
+skit.addEventListener('click', function(){ 
+  btn.classList.add("synligt");
+  btn.classList.remove("osynligt");
+  skit.classList.remove("synligt");
+  skit.classList.add("osynligt");
+});
